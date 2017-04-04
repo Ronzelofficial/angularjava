1938eaa616d23ea06c78aaec387b7f824254e97b
Ronen Reouveni <ronenreouveni@gmail.com>
2017-04-03 19:34:40 -0700

    angular7assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -27,17 +27,24 @@
  
     currentSong = song;
  };
+    
+//         the playsong function plays currentbuzzobject
+         
+   var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+        }
          
     SongPlayer.play = function(song) {
             if (currentSong !== song) {
              
                 
         setSong(song);         
-        currentBuzzObject.play(); 
-        song.playing = true;  
+        playSong(song); 
+//        song.playing = true;  
               } else if (currentSong === song) {
          if (currentBuzzObject.isPaused()) {
-             currentBuzzObject.play();
+             playSong(song);
          }
      } 
         
